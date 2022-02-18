117aa8d2f5952f0ff83bb9858315f5a46676b7ad
楊勇 <yangyong@netstars.co.jp>
2022-02-18 16:03:03 +0900

    Initial Commit

---
+++ Shared/gitsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitsearchApp.swift
+//  Shared
+//
+//  Created by 楊勇 on R 4/02/18.
+//
+
+import SwiftUI
+
+@main
+struct gitsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
